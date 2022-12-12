2dc8b5b61d6cff61c1be218439e568ad4200c189
dalouis01 <94994180+dalouis01@users.noreply.github.com>
2022-12-11 23:39:58 -0500

    ajoute 9

--- Master_FUNCTION/8-Dekripte.py
+++ Master_FUNCTION/8-Dekripte.py
@@ -1,2 +1,5 @@
-
-print(("asdasd").upper())
+import string
+print(int(2))
+print(chr(65+0))
+fonks_dekryp= lambda mo: ''.join(chr(65+int(mo[i])) for i in range(len(mo)))
+print(fonks_dekryp("1-2"))
---
+++ Master_FUNCTION/9-Pemite.py
@@ -0,0 +1,8 @@
+def fonksyon_perm(var1,var2) :
+    var3 =var1
+    var1=var2
+    var2=var3
+    return(var1,var2)
+print("Rezilta apre pemitasyon se ",fonksyon_perm(input("Rantre premye vale a "),input("rantre 2em vale a ")))
+    
+    
